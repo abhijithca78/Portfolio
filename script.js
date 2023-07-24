d81c5a399ff11097562b8fa32f8c8598b997f1a3
abhijithca78 <83497961+abhijithca78@users.noreply.github.com>
2023-07-24 12:27:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text-change", {
+    strings: ["Frontend Developer",
+    "Bench Sales Recruiter"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
